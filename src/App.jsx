d9262a8a1d08f3ca71e0d498cc2c481268ed0e18
SteveLorde <mostafa.maher98@gmail.com>
2023-12-10 05:59:33 +0200

    setup typescript, react, react-dom

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.App = void 0;
+const react_1 = __importDefault(require("react"));
+function App() {
+    return (<div>
+            <h1>Hello, React!</h1>
+        </div>);
+}
+exports.App = App;
